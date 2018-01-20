fca5a2817df039231e421e1b971a231a546685f0
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2018-01-20 13:50:27 -0600

    updates for interactivity between components

--- src/components/client-form.js
+++ src/components/client-form.js
@@ -1,42 +1,51 @@
 import React from 'react';
 import {reduxForm, Field} from 'redux-form';
+import FormCategoryRow from './form-category-row';
+import FormRowInput from './form-row-input';
+import {required, nonEmpty} from '../validators';
 
 export class ClientForm extends React.Component {
     onSubmit(values) {
         console.log(values);
     }
+    //refer to this link for reference https://github.com/Thinkful-Ed/redux-contact-form/blob/master/src/components/contact-form.js
     render() {
+        // let successMessage; Added in for API
+        // let errorMessage;
+        // if (this.props.submitSucceeded) {
+        //     successMessage = (
+        //         <div className="message message-success">
+        //             Message submitted successfully
+        //         </div>
+        //     );
+        // }
+        // if (this.props.error) {
+        //     errorMessage = (
+        //         <div className="message message-error">{this.props.error}</div>
+        //     );
+        // }
+
         return (
             <form
               onSubmit={this.props.handleSubmit(values =>
                 this.onSubmit(values)
-              )}
-            >
+              )}>
+              // {successMessage} Added in for API
+              // {errorMessage}
               <FormCategoryRow title="Client Name"/>
-                <label for="first-name">First Name</label>
-                <Field name="first-name" id="first-name" type="text" component="FormRowInput" />
-                <label for="last-name">Last Name</label>
-                <Field name="last-name" id="last-name" type="text" component="FormRowInput" />
-
+                <Field name="first-name" type="text" label="First Name" component={FormRowInput} validate={[required, nonEmpty]} />
+                <Field name="last-name" type="text" label="Lase Name" component={FormRowInput} validate={[required, nonEmpty]} />
               <FormCategoryRow title="Client Location" />
-                <label for="hospital-name">Hospital Name</label>
-                <Field name="hopsital-name" id="hospital-name" type="text" component="FormRowInput"/>
-                <label for="city">City</label>
-                <Field name="city" id="city" type="text" component="FormRowInput" />
-                <label for="state">State</label>
-                <Field name="state" id="state" type="text" component="FormRowInput" />
-
+                <Field name="hopsital-name" type="text" label="Hospital Name" component={FormRowInput} validate={[required, nonEmpty]} />
+                <Field name="city" type="text" label="City" component={FormRowInput} validate={[required, nonEmpty]} />
+                <Field name="state" type="text" label="State" component={FormRowInput} validate={[required, nonEmpty]} />
               <FormCategoryRow title="Time Frame" />
-                <label for="start-date">Start Date</label>
-                <Field name="start-date" id="start-date" type="date" component="FormRowInput"/>
-                <label for="end-date">End Date</label>
-                <Field name="end-date" id="end-date" type="date" component="FormRowInput"/>
+                <Field name="start-date" label="Start Date" type="date" component={FormRowInput} validate={[required, nonEmpty]} />
+                <Field name="end-date" type="date" label="End Date" component={FormRowInput} validate={[required, nonEmpty]} />
               <FormCategoryRow title="Client Information" />
-                <label for="age">Age</label>
-                <Field name="age" id="age" type="text" component="FormRowInput"/>
-                <label for="weight">Weight</label>
-                <Field name="weight" id="weight" type="text" component="FormRowInput"/>
-                <button type="submit">Update Client</button>
+                <Field name="age" type="text" label="Age" component={FormRowInput} validate={[required, nonEmpty]} />
+                <Field name="weight" type="text" label="Weight" component={FormRowInput} validate={[required, nonEmpty]} />
+                <button type="submit" diabled={this.props.pristine || this.props.submitting}>Update Client</button>
             </form>
         );
     }
