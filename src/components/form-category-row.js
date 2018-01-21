2ff76aaf64c5385d08aa03892a4d7761bc1839eb
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2018-01-21 10:55:35 -0600

    initialize css files

---
+++ src/components/form-category-row.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './form-category-row.css';
+
+export default function FormCategoryRow(props) {
+      return (
+          <h3 className="form-category-row">
+            {props.title}
+          </h3>
+      );
+}
