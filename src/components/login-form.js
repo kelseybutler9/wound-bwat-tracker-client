9edeb5344dbe13c9466578d7222c170adfa8187e
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2018-01-21 13:23:37 -0600

    update log in and sign up pages

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,1 +1,21 @@
-import TopNav from './top-nav';
+import React from 'react';
+import {reduxForm, Field} from 'redux-form';
+import {required, nonEmpty} from '../validators';
+
+export function FormRowDisplay(props) {
+      return (
+        <form>
+          <h1><Welcome to the Wound Care Progression Tool></h1>
+          <h2>Log In</h2>
+          <Field name="username" type="text" label="Username" component={FormRowInput} validate={[required, nonEmpty]} />
+          <Field name="password" type="text" label="Password" component={FormRowInput} validate={[required, nonEmpty]} />
+          <button type="submit" diabled={this.props.pristine || this.props.submitting}>Submit</button>//handle submit and if successful redirect to home page
+          <Link to={`/signup`}>Sign Up</Link>
+        </form>
+      );
+};
+
+FormRowDisplay.defaultProps = {
+    title: '',
+    value: ''
+};
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,1 +1,23 @@
-import TopNav from './top-nav';
+import React from 'react';
+import {reduxForm, Field} from 'redux-form';
+import {required, nonEmpty} from '../validators';
+
+export function FormRowDisplay(props) {
+      return (
+        <form>
+          <h1><Welcome to the Wound Care Progression Tool></h1>
+          <h2>Sign Up</h2>
+          <h3>Enter a valid Username and Password</h3>
+          <p>A valid password will have at least 8 characters</p>
+          <Field name="username" type="text" label="Username" component={FormRowInput} validate={[required, nonEmpty]} />
+          <Field name="password" type="text" label="Password" component={FormRowInput} validate={[required, nonEmpty]} />
+          <button type="submit" diabled={this.props.pristine || this.props.submitting}>Sign Up</button>//handle submit and if successful redirect to home page
+          <button type="submit" diabled={this.props.pristine || this.props.submitting}>Cancel</button>//redirect to log in screen
+        </form>
+      );
+};
+
+FormRowDisplay.defaultProps = {
+    title: '',
+    value: ''
+};
