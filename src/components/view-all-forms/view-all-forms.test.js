41677bdc77b5c096cd32323a213130408b201103
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2018-03-25 19:53:58 -0400

    update view all forms test

--- src/components/view-all-forms/view-all-forms.test.js
+++ src/components/view-all-forms/view-all-forms.test.js
@@ -7,41 +7,42 @@ describe('<ViewAllForms />', () => {
   it('Renders without crashing', () => {
     shallow(<ViewAllForms />);
   });
+
+  it('Should render the forms when client selected', () => {
+          const wrapper = shallow(<ViewAllForms />);
+          //wrapper.find('select').simulate('keyDown', { keyCode: 40 });
+          wrapper.setState({
+            clientSelected: true,
+            clientId: '5a6541b39293b078f266ae57',
+            forms: {
+             "client_id": '5a6541b39293b078f266ae57',
+             "date_of_form": "2018-03-09T06:00:00.000Z",
+             "wound_location": "Heel",
+             "shape_of_wound": "Irregular",
+             "question_one": 1,
+             "question_two": 2,
+             "question_three": 0,
+             "question_four": 4,
+             "question_five": 1,
+             "question_six": 0,
+             "question_seven": 1,
+             "question_eight": 2,
+             "question_nine": 3,
+             "question_ten": 3,
+             "question_eleven": 0,
+             "question_twelve": 0,
+             "question_thirteen": 3,
+             "score": 30
+            }
+          });
+          wrapper.update();
+          expect(wrapper.state('clientSelected')).toEqual(true);
+          expect(wrapper.state('clientId')).isEmpty().to.toEqual(false);
+          expect(wrapper.state('forms')).isEmpty().to.toEqual(false);
+  });
 });
 
-it('Should render the forms when client selected', () => {
-        const wrapper = shallow(<ViewAllForms />);
-        //wrapper.find('select').simulate('keyDown', { keyCode: 40 });
-        wrapper.setState({
-          clientSelected: true,
-          clientId: '5a6541b39293b078f266ae57',
-          forms: {
-           "client_id": '5a6541b39293b078f266ae57',
-           "date_of_form": "2018-03-09T06:00:00.000Z",
-           "wound_location": "Heel",
-           "shape_of_wound": "Irregular",
-           "question_one": 1,
-           "question_two": 2,
-           "question_three": 0,
-           "question_four": 4,
-           "question_five": 1,
-           "question_six": 0,
-           "question_seven": 1,
-           "question_eight": 2,
-           "question_nine": 3,
-           "question_ten": 3,
-           "question_eleven": 0,
-           "question_twelve": 0,
-           "question_thirteen": 3,
-           "score": 30
-          }
-        });
-        wrapper.update();
-        expect(wrapper.state('clientSelected')).toEqual(true);
-        expect(wrapper.hasClass('forms')).toEqual(true);
-        expect(wrapper.state('clientId')).isEmpty().to.toEqual(false);
-        expect(wrapper.state('forms')).isEmpty().to.toEqual(false);
-});
+
 
 // jest.mock('axios', () => {
 //   const clients = [
